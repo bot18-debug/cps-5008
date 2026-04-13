3b96fbeb7a469dbd8c5c18240d5239dc1b9e013a
vinayak singhal <2411066@live.stmarys.ac.uk>
2026-04-13 20:26:46 +0100

    also deleted some folder that were replicated also added some required imports or import used in imports.py

--- imports.py
+++ imports.py
@@ -1,2 +1,20 @@
-import numpy as np 
-import unittest
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import (recall_score, precision_score, f1_score, roc_auc_score,
+                             confusion_matrix, classification_report)
+from imblearn.over_sampling import SMOTE
+from imblearn.pipeline import Pipeline as ImbPipeline
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
